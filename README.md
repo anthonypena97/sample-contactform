a0f97a89bc08b1c87b0996aa9542748ef5441520
Anthony Pena <apena5@alumni.jh.edu>
2022-01-20 19:46:26 -0700

    create-react-app + initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sample-contactform
+
 A Functioning React Contact Form
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Contact from './components/Contact';
+
+// import './App.css';
+
+function App() {
+
+  return (
+
+    <Contact></Contact>
+
+  );
+}
+
+export default App;
