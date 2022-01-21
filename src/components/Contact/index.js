296c54e4db36ab42c687155cded40fed5d600daa
Anthony Pena <apena5@alumni.jh.edu>
2022-01-21 03:39:27 -0700

    added styling + fixed bugs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import { validateEmail, validateDate } from '../../utils/helpers';
 
@@ -110,7 +113,7 @@ function Contact() {
     if (field === 'birthDate') {
       const isValid = validateDate(e.target.value);
       if (!isValid) {
-        setErrorMessage({ ...errorMessage, [field]: 'Please follow yyyy-mm-dd format' });
+        setErrorMessage({ ...errorMessage, [field]: 'Follow yyyy-mm-dd format' });
       } else {
         setErrorMessage({ ...errorMessage, [field]: '' });
         setFormState({ ...formState, [field]: e.target.value });
@@ -141,7 +144,7 @@ function Contact() {
 
   }
 
-  // html for component
+  // =========================================== html for component ============================================
   return (
 
     <div id="contact-form" onSubmit={handleSubmit} className="contactForm" >
@@ -154,74 +157,125 @@ function Contact() {
       {/* ======================== inputs ==================== */}
 
       {/* ======================================== name =================================== */}
-      <div className="contactItem">
-        <label htmlFor="name">Name:</label>
-        <input className="contactInput" type="text" name="name" value={name} onChange={handleChange} onBlur={handleValidation} />
-      </div>
 
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage.name}</p>
-        </div>
-      )}
+      <Container>
+        <Row className="contactRow">
+          <Col variant="input" className="contactInput" style={{ 'padding': '1px' }}>
+
+            <label className="contactLabel" htmlFor="name">Name:</label>
+            <input type="text" name="name" value={name} onChange={handleChange} onBlur={handleValidation} />
+
+          </Col>
+
+          <Col style={{ 'padding': '1px', 'marginTop': 'auto', 'marginBottom': 'auto' }} >
+
+            {errorMessage && (
+              <div>
+                <p className="errorMessage">{errorMessage.name}</p>
+              </div>
+            )}
+
+          </Col>
+        </Row>
+      </Container>
+
 
       {/* ======================================= email =================================== */}
-      <div className="contactItem">
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" value={email} onChange={handleChange} onBlur={handleValidation} />
-      </div>
 
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage.email}</p>
-        </div>
-      )}
+      <Container>
+        <Row className="contactRow">
+          <Col variant="input" className="contactInput" style={{ 'padding': '1px' }}>
+
+            <label className="contactLabel" htmlFor="name">Email:</label>
+            <input type="email" name="email" value={email} onChange={handleChange} onBlur={handleValidation} />
+
+          </Col>
+
+          <Col style={{ 'padding': '1px', 'marginTop': 'auto', 'marginBottom': 'auto' }} >
+
+            {errorMessage && (
+              <div>
+                <p className="errorMessage">{errorMessage.email}</p>
+              </div>
+            )}
+
+          </Col>
+        </Row>
+      </Container>
 
       {/* ====================================== birth date =============================== */}
-      <div className="contactItem">
-        <label htmlFor="message">Birth Date:</label>
-        <input name="birthDate" rows="5" value={birthDate} onChange={handleChange} onBlur={handleValidation} />
-      </div>
 
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage.birthDate}</p>
-        </div>
-      )}
+      <Container>
+        <Row className="contactRow">
+          <Col variant="input" className="contactInput" style={{ 'padding': '1px' }}>
+
+            <label className="contactLabel" htmlFor="name">Birth Date:</label>
+            <input name="birthDate" rows="5" value={birthDate} onChange={handleChange} onBlur={handleValidation} />
+
+          </Col>
+
+          <Col style={{ 'padding': '1px', 'marginTop': 'auto', 'marginBottom': 'auto' }} >
+
+            {errorMessage && (
+              <div>
+                <p className="errorMessage">{errorMessage.birthDate}</p>
+              </div>
+            )}
+
+          </Col>
+        </Row>
+      </Container>
 
       {/* ================================== email consent ================================= */}
-      <div className="contactItem">
-        <input type="checkbox" name="emailConsent" value={emailConsent} checked={emailConsent} onChange={handleChange} onBlur={handleValidation} />
-        <label> I agree to be contacted via email</label>
-      </div>
+      <Container>
+        <Row>
+          <div className="checkboxInput">
+            <input className="checkBox" type="checkbox" name="emailConsent" value={emailConsent} checked={emailConsent} onChange={handleChange} onBlur={handleValidation} />
+            <label className="checkBoxLabel"> I agree to be contacted via email</label>
+          </div>
 
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage.emailConsent}</p>
-        </div>
-      )}
+          {
+            errorMessage && (
+              <div>
+                <p className="errorCheck">{errorMessage.emailConsent}</p>
+              </div>
+            )
+          }
+        </Row>
+      </Container>
+
 
       {/* ======================== buttons ==================== */}
-      <div id="formButtons">
+      <Container>
+        <Row>
 
-        <div className="contactItem submitContact">
-          <button data-testid="button" onClick={clearFormData}>Clear</button>
-        </div>
+          <Col>
+            <div className="contactItem submitContact">
+              <button data-testid="button" onClick={clearFormData}>Clear</button>
+            </div>
+          </Col>
 
-        <div className="contactItem submitContact">
-          <button data-testid="button" type="submit" onClick={handleSubmit}>Submit</button>
-        </div>
+          <Col>
+            <div className="contactItem submitContact">
+              <button data-testid="button" type="submit" onClick={handleSubmit}>Submit</button>
+            </div>
+          </Col>
 
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage.result}</p>
-          </div>
-        )}
+        </Row>
+        <Row>
 
-      </div>
+          <Col>
+            {errorMessage && (
+              <div>
+                <p className="resultText">{errorMessage.result}</p>
+              </div>
+            )}
+          </Col>
 
+        </Row>
+      </Container>
 
-    </div>
+    </div >
 
   )
 
