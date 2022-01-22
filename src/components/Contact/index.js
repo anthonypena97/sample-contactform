996a55c3cb0da7b586950ca78093aedeb5b9f0ee
Anthony Pena <apena5@alumni.jh.edu>
2022-01-22 11:13:50 -0700

    empty field submission bug fixed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -21,9 +21,9 @@ function Contact() {
 
     // checks to make sure there are no empty fields
     if (
-      !formState.name === '' ||
-      !formState.email === '' ||
-      !formState.birthDate === '' ||
+      !formState.name === '' &&
+      !formState.email === '' &&
+      !formState.birthDate === '' &&
       !formState.emailConsent === false
     ) {
       // if no empty fields, checks if all inputs are valid
@@ -132,13 +132,13 @@ function Contact() {
   const postAPI = (data) => {
     console.log(data);
 
-    axios.post('https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users', data)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    // axios.post('https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users', data)
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
 
   }
 
