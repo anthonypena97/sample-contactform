b8d4bf25c6a2bb6abb0b961e58314cf9a80dff61
Anthony Pena <apena5@alumni.jh.edu>
2022-01-24 04:24:28 -0700

    mobile css update + checkbox validation updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import Header from './components/Header';
 import Contact from './components/Contact';
-
-// import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [height, setHeight] = React.useState(window.innerHeight);
+  const [vh, setVh] = React.useState(window.innerHeight * 0.01);
+
+
+  React.useEffect(() => {
+    window.addEventListener("resize", updateWidthAndHeight);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    return () => window.removeEventListener("resize", updateWidthAndHeight);
+
+  });
+
+  const updateWidthAndHeight = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+    setVh(window.innerHeight * 0.01);
+
+  };
+
+
+  window.addEventListener('wheel', e => {
+    e.preventDefault();
+  }, { passive: false });
+
+  // preventss user from scaling app
+  window.addEventListener(
+    "touchmove",
+    function (event) {
+      if (event.scale !== 1) {
+        event.preventDefault();
+      }
+    },
+    { passive: false }
+  );
+
+  window.addEventListener('gesturestart', e => e.preventDefault());
+  window.addEventListener('gesturechange', e => e.preventDefault());
+  window.addEventListener('gestureend', e => e.preventDefault());
+
   return (
 
-    <div>
+
+    <div id="application">
+
+      {/* for debugging */}
+      {/* <div>{`1Window width = ${width}`}</div>
+      <div>{`1Window height = ${height}`}</div> */}
 
       <Header></Header>
 
